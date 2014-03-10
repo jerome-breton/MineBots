305bda1f8991b512ed0d6f9717da1bb9b30e7614
Jérôme Breton <jbreton@sqli.com>
2014-03-10 09:17:27 +0100

    Remise en place des imports poiur les anotation dans ViewController

--- src/MineRobot/GameBundle/Controller/ViewController.php
+++ src/MineRobot/GameBundle/Controller/ViewController.php
@@ -5,6 +5,8 @@ namespace MineRobot\GameBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 // these import the "@Route" and "@Template" annotations
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class ViewController extends Controller
 {
